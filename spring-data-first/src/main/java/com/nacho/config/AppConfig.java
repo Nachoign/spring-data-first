51cd1abd37b43050d3df3fda0fedebd75b28bf60
igonzalez <igonzalez@192.168.1.102>
2015-03-23 09:24:07 -0500

    This changes contain the new dependencie lombok that will manage the setters and getters, the new entity Book

--- spring-data-first/src/main/java/com/nacho/config/AppConfig.java
+++ spring-data-first/src/main/java/com/nacho/config/AppConfig.java
@@ -1,13 +1,25 @@
 package com.nacho.config;
 
+import static org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO;
+
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.stereotype.Controller;
 
 @ComponentScan(basePackages = "com.nacho", excludeFilters = {
@@ -25,4 +37,48 @@ public class AppConfig extends RepositoryRestMvcConfiguration {
 		}
 	}
 
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabase(Database.MYSQL);
+		return adapter;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws ClassNotFoundException {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setDataSource(dataSource());
+		factoryBean.setPackagesToScan("com.nacho.model");
+		factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+		factoryBean.setJpaProperties(jpaProperties());
+
+		return factoryBean;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager() throws ClassNotFoundException {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
+		return transactionManager;
+	}
+
+	@Bean
+	public Properties jpaProperties() {
+		Properties properties = new Properties();
+		properties.put(HBM2DDL_AUTO, "update"); // It's better to don't have this in production
+		return properties;
+	}
 }
