0c474964baa90e022dbef4aab496fc877f45a7f0
igonzalez <igonzalez@192.168.1.102>
2015-03-23 16:58:20 -0500

    More search methods:
    findByTittle
    findByTittleLike
    findByTittleContaining

--- spring-data-first/src/main/java/com/nacho/repository/BookRepository.java
+++ spring-data-first/src/main/java/com/nacho/repository/BookRepository.java
@@ -1,11 +1,18 @@
 package com.nacho.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.nacho.model.Book;
 
 @RepositoryRestResource(collectionResourceRel = "books", path = "books")
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+	
+	List<Book> findByTittle(@Param("tittle") String tittle);
+	List<Book> findByTittleLike(@Param("tittle") String tittle);
+	List<Book> findByTittleContaining(@Param("tittle") String tittle);
 
 }
